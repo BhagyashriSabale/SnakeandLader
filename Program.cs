03edb20249ab5fd695afbace479dc34d294bc23c
BhagyashriSabale <balisabale16@gmail.com>
2023-04-13 00:29:44 +0530

    [Bhagyashri] welcome msg

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace SnakeandLader
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to the Snake and Ladder game");
+        }
+    }
+}
